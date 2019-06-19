04d250d0cd52d2bb123387898c05cd39e909414b
feuler <feuler@feulers-MacBook-Air.local>
2019-06-19 00:11:04 -0400

    finished project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+#title
+    title = models.CharField(max_length=255)
+#pub_date
+    pub_date = models.DateTimeField()
+#body
+    body = models.TextField()
+#image
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y') 
+
+#Add the blog app to the settings
+
+#create a migration
+
+# Migrate
+
+#Add to the Main
